46a4eddbc484b89fdaf6812443e1e12fef3f4476
Jordan McQueen <dojo3@uw.edu>
2017-09-13 05:24:24 -0700

    syntax + naming minor changes

--- actualizer/log/client.py
+++ actualizer/log/client.py
@@ -1,18 +1,19 @@
 from typing import List
 import datetime
 import re
-import util
-import base
+
+from actualizer import util
+from actualizer.log import base
 
 LOG_SUBCLASSES = util.get_all_subclasses(base.Log)
 
-def factory(log_request_context: dict) -> Log:
+def factory(log_request_context: dict) -> base.Log:
     kind = get_log_type(log['message'])
     return kind(log_request_context)
 
 def get_log_type(message: str) -> type:
     for cls in LOG_SUBCLASSES:
-        if cls.LOG_PATTERN.match(message):
+        if cls.MESSAGE_PATTERN.match(message):
             return cls
     return None
 
