cb9193ce463a24004619a801cc1230d6a06bc84b
Jordan McQueen <dojo3@uw.edu>
2017-09-13 03:14:05 -0700

    utils

---
+++ util.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def get_all_subclasses(cls: type) -> List[type]:
+    all_subclasses = []
+
+    for subclass in cls.__subclasses__():
+        all_subclasses.append(subclass)
+        all_subclasses.extend(get_all_subclasses(subclass))
+
+    return all_subclasses
+
