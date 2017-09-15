41badda21c0789f5e8388f2dc4f210312975b2eb
Jordan McQueen <dojo3@uw.edu>
2017-09-14 23:43:15 -0700

    generated ddb types based on type signatures

--- actualizer/dao.py
+++ actualizer/dao.py
@@ -1,17 +1,24 @@
 import boto3
 import datetime
-from typing import List
-from actualizer.log.base import Log
+from typing import List, Union
+from inspect import signature
+from actualizer import log
+from actualizer import util
 from boto3.dynamodb.conditions import Key, Attr
 
 LOG_TABLE_NAME_TEMPLATE = 'actualizer-{region}-{domain}-logs'
-ATTRIBUTE_TYPE_MAPPING = {
-        'message': 'S',
-        'datetime': 'S',
-        'username': 'S',
-        'request_time': 'S',
-        'food': 'S',
-        'calories': 'N'
+LOG_SUBCLASSES = [x for x in util.get_all_subclasses(log.base.Log)]
+FIELD_SERIALIZERS = {k:v for x in LOG_SUBCLASSES for k, v in x.FIELD_SERIALIZER.items()}
+
+# mapping from field name to serializer function return type
+RETURN_SIGNATURES_FOR_FIELDS = {k:signature(v).return_annotation for k, v in FIELD_SERIALIZERS.items()}
+
+# mapping from return types to DDB string codes for that type
+DDB_ATTRIBUTE_TYPE_MAPPING = {
+        str: 'S',
+        Union[int, float]: 'N',
+        int: 'N',
+        float: 'N'
         }
 
 class LogTableDao:
@@ -24,22 +31,23 @@ class LogTableDao:
         self.ddb = self.session.resource('dynamodb')
         self.table = self.ddb.Table(self.table_name)
 
-    def save(self, log: Log) -> None:
-        itemdict = {x: getattr(x) for x in log._FIELDS}
-        ddb.put_item(
+    def save(self, log: log.base.Log) -> None:
+        itemdict = log.to_serialized_dict()
+        put_response = ddb.put_item(
                 TableName = self.table_name,
                 Item = convert_dict_to_ddb_item(itemdict)
                 )
+        return put_response
 
-def list_by_timerange(self, username: str, start: datetime.datetime,
-                      end: datetime.datetime) -> List[dict]:
-    response = self.table.query(
-            KeyConditionExpression = Key('username').eq(username) & \
-            Key('datetime').gt(start) & \
-            Key('datetime').lt(end)
-            )
-    return response
+    def list_by_timerange(self, username: str, start: datetime.datetime,
+                          end: datetime.datetime) -> List[dict]:
+        response = self.table.query(
+                KeyConditionExpression = Key('username').eq(username) & \
+                Key('datetime').gt(start) & \
+                Key('datetime').lt(end)
+                )
+        return response
 
 def convert_dict_to_ddb_item(data: dict) -> dict:
-    return {k:{ATTRIBUTE_TYPE_MAPPING[k]: v} for k, v in data.items()}
+    return {k:{DDB_ATTRIBUTE_TYPE_MAPPING[RETURN_SIGNATURES_FOR_FIELDS[k]]: v} for k, v in data.items()}
 
