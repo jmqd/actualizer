f7e504aa3d49dd3d1105296806d01d22da6544a3
Jordan McQueen <dojo3@uw.edu>
2017-09-14 23:44:56 -0700

    store all serializing functions for logs

---
+++ actualizer/log/serializers.py
@@ -0,0 +1,21 @@
+import datetime
+from typing import Union
+from actualizer import util
+
+def serialize_username(username: str) -> str:
+    return username
+
+def serialize_message(message: str) -> str:
+    return message
+
+def serialize_request_time(request_time: datetime.datetime) -> str:
+    return request_time.isoformat()
+
+def serialize_datetime(_datetime: datetime.datetime) -> str:
+    return _datetime.isoformat()
+
+def serialize_calories(calories: Union[int, float, str]) -> Union[int, float]:
+    return util.convert_numeric(calories)
+
+def serialize_food(food: str) -> str:
+    return str(food)
