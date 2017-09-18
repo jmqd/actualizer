ccfee5ee8640667f292a52223ad285316eded15d
Jordan McQueen <dojo3@uw.edu>
2017-09-17 20:44:55 -0700

    generalize ddb dao + create PotentialsTableDao

--- actualizer/dao.py
+++ actualizer/dao.py
@@ -7,6 +7,8 @@ from actualizer import util
 from boto3.dynamodb.conditions import Key, Attr
 
 LOG_TABLE_NAME_TEMPLATE = 'actualizer-{region}-{domain}-logs'
+POTENTIAL_TABLE_NAME_TEMPLATE = 'actualizer-{region}-{domain}-potentials'
+
 LOG_SUBCLASSES = [x for x in util.get_all_subclasses(log.base.Log)]
 FIELD_SERIALIZERS = {k:v for x in LOG_SUBCLASSES for k, v in x.FIELD_SERIALIZER.items()}
 
@@ -20,23 +22,26 @@ DDB_ATTRIBUTE_TYPE_MAPPING = {
         float: 'N'
         }
 
-class LogTableDao:
-    def __init__(self, region: str, domain: str) -> 'LogTableDao':
+class DdbTableDao:
+    def __init__(self, region: str, domain: str) -> None:
         self.region = region
         self.domain = domain
-        self.table_name = LOG_TABLE_NAME_TEMPLATE.format(region=region, domain=domain)
+        self.table_name = self.TABLE_NAME_TEMPLATE.format(region=region, domain=domain)
         self.session = boto3.session.Session(region_name = self.region)
         self.client = self.session.client('dynamodb')
         self.ddb = self.session.resource('dynamodb')
         self.table = self.ddb.Table(self.table_name)
 
-    def save(self, log: log.base.Log) -> None:
-        itemdict = log.to_serialized_dict()
+    def save(self, serializable_entity: object) -> None:
+        itemdict = serializable_entity.to_serialized_dict()
         put_response = self.client.put_item(
                 TableName = self.table_name,
                 Item = convert_dict_to_ddb_item(itemdict)
                 )
         return put_response
+
+class LogTableDao(DdbTableDao):
+    TABLE_NAME_TEMPLATE = LOG_TABLE_NAME_TEMPLATE
 
     def query_by_timerange(self, username: str, start: datetime.datetime,
                           end: datetime.datetime) -> List[dict]:
@@ -47,7 +52,7 @@ class LogTableDao:
         return response
 
 class NutritionLogDaoHelper:
-    def __init__(self, dao: LogTableDao, context: dict):
+    def __init__(self, dao: LogTableDao, context: dict) -> None:
         self.dao = dao
         self.context = context
 
@@ -58,6 +63,9 @@ class NutritionLogDaoHelper:
         items = response['Items']
         calories = sum([x.get('calories', 0) for x in items])
         return calories
+
+class PotentialTableDao(DdbTableDao):
+    TABLE_NAME_TEMPLATE = POTENTIAL_TABLE_NAME_TEMPLATE
 
 def convert_dict_to_ddb_item(data: dict) -> dict:
     return {k:{DDB_ATTRIBUTE_TYPE_MAPPING[RETURN_SIGNATURES_FOR_FIELDS[k]]: v} for k, v in data.items()}
